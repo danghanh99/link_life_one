209974dc69a6d49e627263f9f6a0b476dbc75d97
danghanh99 <ldhanh1999@gmail.com>
2023-02-08 10:32:07 +0700

    Update  constant

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,4 +1,6 @@
 class Constant {
-  // static const String url = "https://koji-app2.starboardasiavn.com/";
-  static const String url = "https://koji-app.starboardasiavn.com/";
+  //prod
+  static const String url = "https://koji-app2.starboardasiavn.com/";
+  //dev
+  // static const String url = "https://koji-app.starboardasiavn.com/";
 }
