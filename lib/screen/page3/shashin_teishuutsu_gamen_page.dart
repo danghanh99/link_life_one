1f06b0eb8b8dcf8c206da87f165e7884e2a919bc
Thanh Bui <thanhbpc@voyager-hcm.com>
2023-03-14 11:49:30 +0700

    Fix 285-7

--- lib/screen/page3/shashin_teishuutsu_gamen_page.dart
+++ lib/screen/page3/shashin_teishuutsu_gamen_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:link_life_one/api/koji/postPhotoSubmissionRegistration/upload_photo_api.dart';
@@ -48,64 +47,62 @@ class _ShashinTeishuutsuGamenPageState
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    double imageContainerHeight = (size.height * 2 / 3).sp;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 80,
-                      child: TextLineDown(
-                        style: const TextStyle(
-                            fontSize: 20,
-                            color: Color(0xFF042C5C),
-                            fontWeight: FontWeight.w500),
-                        text: '戻る',
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                      ),
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 80,
+                    child: TextLineDown(
+                      style: const TextStyle(
+                          fontSize: 20,
+                          color: Color(0xFF042C5C),
+                          fontWeight: FontWeight.w500),
+                      text: '戻る',
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
                     ),
-                    Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: const Color.fromARGB(255, 247, 240, 240))),
-                        width: 200,
-                        child: CustomButton(
-                          color: Colors.white70,
-                          onClick: () {
-                            print("object");
-                          },
-                          name: '写真提出画面',
-                          textStyle: const TextStyle(
-                            color: Color(0xFF042C5C),
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                          ),
+                  ),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromARGB(255, 247, 240, 240))),
+                      width: 200,
+                      child: CustomButton(
+                        color: Colors.white70,
+                        onClick: () {
+                          print("object");
+                        },
+                        name: '写真提出画面',
+                        textStyle: const TextStyle(
+                          color: Color(0xFF042C5C),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
                     ),
-                    Container(
-                      width: 80,
-                    )
-                  ],
-                ),
-                const SizedBox(height: 50),
-                Container(
+                  ),
+                  Container(
+                    width: 80,
+                  )
+                ],
+              ),
+              const SizedBox(height: 50),
+              Expanded(
+                child: Container(
                   width: size.width - 50,
-                  height: imageContainerHeight,
                   padding: const EdgeInsets.all(5),
                   decoration:
                       BoxDecoration(border: Border.all(color: Colors.black)),
@@ -116,8 +113,8 @@ class _ShashinTeishuutsuGamenPageState
                       children: [
                         ...selectedImages.map((e) {
                           return SizedBox(
-                            width: (imageContainerHeight / 4) - 5,
-                            height: (imageContainerHeight / 4) - 5,
+                            width: ((size.width - 60)) / 6 - 5,
+                            height: ((size.width - 60)) / 6 - 5,
                             child: Stack(
                               children: [
                                 Positioned.fill(
@@ -130,8 +127,8 @@ class _ShashinTeishuutsuGamenPageState
                                     top: 0,
                                     right: 0,
                                     child: Padding(
-                                      padding:
-                                          const EdgeInsets.only(top: 5, right: 5),
+                                      padding: const EdgeInsets.only(
+                                          top: 5, right: 5),
                                       child: IconButton(
                                           onPressed: () {
                                             setState(() {
@@ -153,15 +150,76 @@ class _ShashinTeishuutsuGamenPageState
                     ),
                   ),
                 ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 18),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 18),
+                    child: GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: Container(
+                        width: 150,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1.5),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            '写真を添付',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w300),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 18),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).popUntil((route) =>
+                            route.settings.name == 'KojiichiranPage3');
+                      },
                       child: GestureDetector(
-                        onTap: () {
-                          selectImage();
+                        onTap: () async {
+                          final box = await Hive.openBox<String>('user');
+                          String loginID = box.values.last;
+
+                          if (selectedImages.isEmpty) {
+                            CustomToast.show(
+                              context,
+                              message: "写真を選択してください。。",
+                              backGround: Colors.orange,
+                            );
+                          } else {
+                            UploadPhotoApi().uploadPhotoApi(
+                                JYUCYU_ID: widget.JYUCYU_ID,
+                                LOGIN_ID: loginID,
+                                FILE_PATH_LIST:
+                                    selectedImages.map((e) => e.path).toList(),
+                                onFailed: () {
+                                  CustomToast.show(
+                                    context,
+                                    message: "写真のアップロードに失敗しました。。",
+                                    backGround: Colors.red,
+                                  );
+                                },
+                                onSuccess: () {
+                                  CustomToast.show(
+                                    context,
+                                    message: "写真をアップロード出来ました。",
+                                    backGround: Colors.green,
+                                  );
+                                  Navigator.of(context).popUntil((route) =>
+                                      route.settings.name ==
+                                      'KojiichiranPage3');
+                                });
+                          }
                         },
                         child: Container(
                           width: 150,
@@ -171,7 +229,7 @@ class _ShashinTeishuutsuGamenPageState
                           ),
                           child: const Center(
                             child: Text(
-                              '写真を添付',
+                              '登録',
                               style: TextStyle(
                                   fontSize: 20, fontWeight: FontWeight.w300),
                             ),
@@ -179,85 +237,11 @@ class _ShashinTeishuutsuGamenPageState
                         ),
                       ),
                     ),
-                  ],
-                ),
-                SizedBox(height: size.width > size.height ? 20 : 50),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 18),
-                      child: GestureDetector(
-                        onTap: () {
-                          // Navigator.pushReplacement(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) =>
-                          //         KojiichiranPage3BaoCaoHoanThanhCongTrinh(
-                          //       initialDate: widget.initialDate,
-                          //     ),
-                          //   ),
-                          // );
-                          Navigator.of(context).popUntil((route) =>
-                              route.settings.name == 'KojiichiranPage3');
-                        },
-                        child: GestureDetector(
-                          onTap: () async {
-                            final box = await Hive.openBox<String>('user');
-                            String loginID = box.values.last;
-      
-                            if (selectedImages.isEmpty) {
-                              CustomToast.show(
-                                context,
-                                message: "写真を選択してください。。",
-                                backGround: Colors.orange,
-                              );
-                            } else {
-                              UploadPhotoApi().uploadPhotoApi(
-                                  JYUCYU_ID: widget.JYUCYU_ID,
-                                  LOGIN_ID: loginID,
-                                  FILE_PATH_LIST: selectedImages.map((e) => e.path).toList(),
-                                  onFailed: () {
-                                    CustomToast.show(
-                                      context,
-                                      message: "写真のアップロードに失敗しました。。",
-                                      backGround: Colors.red,
-                                    );
-                                  },
-                                  onSuccess: () {
-                                    CustomToast.show(
-                                      context,
-                                      message: "写真をアップロード出来ました。",
-                                      backGround: Colors.green,
-                                    );
-                                    Navigator.of(context).popUntil((route) =>
-                                        route.settings.name ==
-                                        'KojiichiranPage3');
-                                  });
-                            }
-                          },
-                          child: Container(
-                            width: 150,
-                            height: 40,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 1.5),
-                            ),
-                            child: const Center(
-                              child: Text(
-                                '登録',
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w300),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+            ],
           ),
         ),
       ),
